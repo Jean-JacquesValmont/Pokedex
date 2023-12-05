8522b772b2e1471bf3a694e95bf03020a0bc6542
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-12-05 17:37:08 +0100

    open and close pokedex tailwinds CSS Clear

--- app/page.tsx
+++ app/page.tsx
@@ -14,19 +14,23 @@ export default function Home() {
 
   return (
     <main className="flex items-center justify-center bg-black">
-    {isOpenPokedex ? (
+    {isOpenPokedex == false ? (
       <div className="relative text-black text-bold text-5xl">
-        <img className="w-full" src="/image/Pokedex_ferme.png" alt="Description de l'image" />
-        <button onClick={closePokedex} className="rounded-full hover:text-gray-500 py-20 px-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[130px]">
-          {"Close"}
+        <img className="w-full" src="/image/Pokedex_ferme.png" alt="pokedex close" />
+        <button onClick={openPokedex} className="rounded-full hover:text-gray-600 py-20 px-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[130px]">
+          {"Open"}
         </button>
       </div>
     ) : (
       <div className="relative text-black text-bold text-5xl">
-        <img className="w-full" src="/image/Pokedex_ferme.png" alt="Description de l'image" />
-        <button onClick={openPokedex} className="rounded-full hover:text-gray-500 py-20 px-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[130px]">
-          {"Open"}
+        <img className="w-full" src="/image/Pokedex_haut_rogner.png" alt="pokedex top" />
+        <button onClick={closePokedex} className="rounded-full hover:text-gray-600 py-12 px-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[430px]">
+          {"Close"}
         </button>
+        <div className="h-[33rem]">
+          <p className="text-white">Test</p>
+        </div>
+        <img className="w-full" src="/image/Pokedex_bas_rogner.png" alt="pokedex bottom" />
       </div>
     )}
   </main>
