ec83cadc1e952c1f49d75ace0ff2f6350a4aa0af
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-12-06 12:19:11 +0100

    Display pokemon in screen after fetch API

---
+++ app/components/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = (props : any) => {
+  return (
+    <div className="text-center shadow-md border-cyan-200 border-2 rounded-lg transform hover:scale-105 transition-transform">
+        <img src={props.item.image} alt={"image de " + props.item.name} />
+        <h1 className="font-bold text-sm">{props.item.name}</h1>
+        {/* <h2>Type1 : {props.item.apiTypes[0]?.name}</h2>
+        {props.item.apiTypes[1]?.name == undefined ? "" : <h2>Type2 : {props.item.apiTypes[1]?.name}</h2>}
+        <p>Evolue en : {props.item.apiEvolutions[0]?.name == "none" || props.item.apiEvolutions[0]?.name == undefined ? "Evolution final" : props.item.apiEvolutions[0]?.name}</p> */}
+    </div>
+  )
+}
+
+export default Card
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,13 @@
 import React, { useState } from "react"
 import Header from './components/Header'
 import Footer from './components/Footer'
+import useFetch from './CustomHooks/UseFetch'
+import Card from "./components/Card"
 
 export default function Home() {
   const [isOpenPokedex, setIsOpenPokedex] = useState(false)
+  const dataPokemon = useFetch('https://pokebuildapi.fr/api/v1/pokemon/generation/1')
+  console.log(dataPokemon)
 
   const openPokedex = () => {
     setIsOpenPokedex(true)
@@ -13,6 +17,14 @@ export default function Home() {
   const closePokedex = () => {
     setIsOpenPokedex(false)
   }
+
+  const cardsPokemon = dataPokemon.map(item =>{
+    return(
+      <Card
+        item = {item}
+      />
+    )
+  })
 
   return (
     <main className="flex items-center justify-center bg-black">
@@ -29,9 +41,11 @@ export default function Home() {
         <button onClick={closePokedex} className="text-5xl rounded-full hover:text-gray-600 py-12 px-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[430px]">
           {"Close"}
         </button>
-        <div className="h-[33rem] bg-gradient-to-b from-blue-200 via-blue-600 to-blue-200 rounded-3xl">
+        <div className="w-full h-[33rem] bg-gradient-to-b from-blue-200 via-blue-600 to-blue-200 rounded-3xl">
           <Header />
-          <p className="text-center">Test</p>
+          <div className="max-w-[35rem] max-h-[21rem] m-4 grid grid-cols-6 gap-1 overflow-y-auto">
+            {cardsPokemon}
+          </div>
           <Footer />
         </div>
         <img className="w-full" src="/image/Pokedex_bas_rogner.png" alt="pokedex bottom" />
