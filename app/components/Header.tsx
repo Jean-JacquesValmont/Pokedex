c680c3cf79b2cc23baa38ca67745c3db6a4fd069
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-12-07 19:31:39 +0100

    Probleme quote in Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react"
 const Header = () => {
   return (
     <header className="text-center font-bold border-b-4 border-cyan-200">
-        Pokedex d"après l'API Pokebuild : https://pokebuildapi.fr/api/v1
+        {"Pokedex d'après l'API Pokebuild : https://pokebuildapi.fr/api/v1"}
     </header>
   )
 }
